d8a184ea3f3ec0103d4532b5870d06d6548cfa68
Daniel Medina <daniel@medina.com>
2021-01-06 13:09:59 -0600

    Refactored small application to use styled-components and removed unused code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Dnrm from './Dnrm';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  flex-direction: column;
+  padding: 0;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Dnrm />
-    </div>
+    </Container>
   );
 }
 
--- src/Dnrm.js
+++ src/Dnrm.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
 import './Dnrm.css';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    font-size: 8em;
+    font-weight: 900;
+    text-align: center;
+    padding: 0;
+`;
+
+const Subheading = styled.h3`
+    font-size: 2em;
+    text-align: center;
+`;
 
 function Dnrm() {
     return (
-        <div className="dnrm-component">
-            <div className="container">
-                <img src={logo} alt="" />
-                <h1>Webpack</h1>
-            </div>
-        </div>
+        <>
+            <Title className="gradient-text">Daniel Medina</Title>
+            <Subheading className="text-3xl text-center sm:text-3xl lg:text-4xl">Web Developer</Subheading>
+        </>
     )
 }
 
