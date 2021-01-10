43adf2a8c5422635490f6519d3ad211a65580aca
Daniel Medina <daniel@medina.com>
2021-01-09 22:22:58 -0600

    Experimented with useState hook, conditional rendering, styled components, and react fragments

--- src/Dnrm.js
+++ src/Dnrm.js
@@ -1,4 +1,4 @@
-import './Dnrm.css';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 
 const Title = styled.h1`
@@ -8,16 +8,44 @@ const Title = styled.h1`
     padding: 0;
 `;
 
-const Subheading = styled.h3`
-    font-size: 2em;
-    text-align: center;
+const Paragraph = styled.p`
+    font-size: 1.2em;
+`;
+
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    border-radius: 5px;
+    margin: 5px;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    padding: 10px;
 `;
 
 function Dnrm() {
+
+    const [count, setCount] = useState(0);
+
+    const plusOne = (event) => {
+        setCount(count + 1);
+    }
+
+    const minusOne = () => {
+        setCount(count - 1);
+    }
+
     return (
         <>
-            <Title className="gradient-text">Daniel Medina</Title>
-            <Subheading className="text-3xl text-center sm:text-3xl lg:text-4xl">Web Developer</Subheading>
+            <Title className="gradient-text">ReactJS</Title>
+            <Paragraph>{count} {count === 69 ? 'nice' : ''}</Paragraph>
+            <ButtonContainer>
+                <Button onClick={minusOne} tabIndex="1">-1</Button>
+                <Button onClick={plusOne} tabIndex="2">+1</Button>
+            </ButtonContainer>
         </>
     )
 }
