0aebb8edf8daf6d65b743da491fc94316e358523
Daniel Medina <daniel@medina.com>
2021-03-17 11:48:21 -0600

    Added a link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,6 +12,7 @@ export default function Footer() {
     return (
         <Container>
             <h1>Made by Daniel Medina</h1>
+            <a href="https://github.com/dnrm">My GitHub</a>
         </Container>
     )
 }
