a80f51ffcd3f07ff63dbd7103fd13483d1e60360
Daniel Medina <daniel@medina.com>
2021-04-09 17:25:22 -0500

    Added the delete request method to /effect page

--- src/pages/Effect.js
+++ src/pages/Effect.js
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useEffect, useState } from "react";
-import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
-import '../styles/users.css';
+import "../styles/users.css";
 
 export default function Effect() {
   const [data, setData] = useState();
@@ -11,8 +11,23 @@ export default function Effect() {
   useEffect(() => {
     fetch("https://faunadb.herokuapp.com/get-users")
       .then((data) => data.json())
-      .then((items) => setData(items["data"]))
+      .then((items) => setData(items["data"]));
   }, []);
+
+  const deleteUser = (e) => {
+    let id = e.target.parentNode.value
+    return fetch("https://faunadb.herokuapp.com/delete-user/" + id, {
+      method: 'delete'
+    }).then(response => response.json());
+  };
+
+  const isAdmin = () => {
+    let key = localStorage.getItem('key');
+    if (key === 'admin') {
+      return true;
+    }
+    return false;
+  }
 
   return (
     <div>
@@ -21,31 +36,37 @@ export default function Effect() {
       </Helmet>
       {data ? (
         <>
-          <h1 style={{ fontSize: '3em' }}>Users</h1>
+          <h1 style={{ fontSize: "3em" }}>Users</h1>
           <br />
           <hr />
           <br />
           {data.map((i) => {
-
-            let name = i["data"]["name"] + ' ' + i["data"]["lastname"];
+            let name = i["data"]["name"] + " " + i["data"]["lastname"];
             let ref = i["ref"]["@ref"]["id"];
 
             return (
               <>
                 <div className="user" key={ref}>
                   <div className="image">
-                    <img src={i["data"]["img"]} alt={`${name}'s profile picture.`} className="profile-pic"/>
+                    <img
+                      src={i["data"]["img"]}
+                      alt={`${name}'s profile picture.`}
+                      className="profile-pic"
+                    />
                   </div>
                   <div className="text">
                     <h1>
-                      <Link to={`/details/${ref}`} className="details">{name}</Link>
+                      <Link to={`/details/${ref}`} className="details">
+                        {name}
+                      </Link>
                     </h1>
                     <h3>{i["data"]["birthDate"]}</h3>
                   </div>
+                  {isAdmin() && <button className="delete-button" value={ref} onClick={(e) => deleteUser(e)}><i className="fas fa-trash"></i></button>}
                 </div>
                 <br />
               </>
-            )
+            );
           })}
         </>
       ) : (
