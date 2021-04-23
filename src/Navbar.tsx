0ba74b168b0c65cbe20c0bc3aa38dce28b31cf42
Daniel Medina <daniel@medina.com>
2021-04-23 13:34:12 -0500

    Added type declarations to useIsAdmin hook

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -8,110 +8,116 @@ import StateTest from "./pages/StateTest";
 import Effect from "./pages/Effect";
 import Coldplay from "./pages/Coldplay";
 import Hidden from "./pages/Hidden";
-import Package from './pages/Package';
-import Details from './pages/Details';
-import Album from './pages/Album';
+import Package from "./pages/Package";
+import Details from "./pages/Details";
+import Album from "./pages/Album";
 
 import styled from "styled-components";
 
 import "./styles/navbar.css";
 
 const Container = styled.div`
-  padding: 50px;
-  color: #92593a;
+    padding: 50px;
+    color: #92593a;
 `;
 
-export default function Navbar() {
-  useEffect(() => {
-    document.querySelector(".navbar-toggle").addEventListener("click", () => {
-      document.querySelector(".main-nav").classList.toggle("active");
-    });
+export default function Navbar(): React.ReactElement {
+    useEffect(() => {
+        document
+            .querySelector(".navbar-toggle")!
+            .addEventListener("click", () => {
+                document.querySelector(".main-nav")!.classList.toggle("active");
+            })!;
 
-    return () => {
-      document.querySelector(".navbar-toggle").removeEventListener("click", () => {
-        document.querySelector(".main-nav").classList.toggle("active");
-      });
-    }
-  }, []);
+        return () => {
+            document
+                .querySelector(".navbar-toggle")!
+                .removeEventListener("click", () => {
+                    document
+                        .querySelector(".main-nav")!
+                        .classList.toggle("active");
+                });
+        };
+    }, []);
 
-  return (
-    <Router>
-      <nav className="navbar">
-        <span className="navbar-toggle" id="js-navbar-toggle">
-          <i className="fas fa-bars"></i>
-        </span>
-        <Link to="/" className="logo">
-          Home
-        </Link>
-        <ul className="main-nav">
-          <li>
-            <Link to="/dnrm" className="nav-links">
-              Dnrm
-            </Link>
-          </li>
-          <li>
-            <Link to="/state-test" className="nav-links">
-              State Test
-            </Link>
-          </li>
-          <li>
-            <Link to="/add-to-array" className="nav-links">
-              Add to array
-            </Link>
-          </li>
-          <li>
-            <Link to="/effect" className="nav-links">
-              useEffect Test
-            </Link>
-          </li>
-          <li>
-            <Link to="/coldplay" className="nav-links">
-              Coldplay
-            </Link>
-          </li>
-          <li>
-            <Link to="/package" className="nav-links">
-              Package
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      {/* A <Switch> looks through its children <Route>s and
+    return (
+        <Router>
+            <nav className="navbar">
+                <span className="navbar-toggle" id="js-navbar-toggle">
+                    <i className="fas fa-bars"></i>
+                </span>
+                <Link to="/" className="logo">
+                    Home
+                </Link>
+                <ul className="main-nav">
+                    <li>
+                        <Link to="/dnrm" className="nav-links">
+                            Dnrm
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/state-test" className="nav-links">
+                            State Test
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/add-to-array" className="nav-links">
+                            Add to array
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/effect" className="nav-links">
+                            useEffect Test
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/coldplay" className="nav-links">
+                            Coldplay
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/package" className="nav-links">
+                            Package
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            {/* A <Switch> looks through its children <Route>s and
                     renders the first one that matches the current URL. */}
-      <Container className="main-content">
-        <Switch>
-          <Route path="/dnrm">
-            <Dnrm />
-          </Route>
-          <Route path="/add-to-array">
-            <AddToArray />
-          </Route>
-          <Route path="/state-test">
-            <StateTest />
-          </Route>
-          <Route path="/effect">
-            <Effect />
-          </Route>
-          <Route path="/coldplay">
-            <Coldplay />
-          </Route>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/hidden">
-            <Hidden />
-          </Route>
-          <Route path="/package">
-            <Package />
-          </Route>
-          <Route path="/album/:name">
-            <Album />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
-  );
+            <Container className="main-content">
+                <Switch>
+                    <Route path="/dnrm">
+                        <Dnrm />
+                    </Route>
+                    <Route path="/add-to-array">
+                        <AddToArray />
+                    </Route>
+                    <Route path="/state-test">
+                        <StateTest />
+                    </Route>
+                    <Route path="/effect">
+                        <Effect />
+                    </Route>
+                    <Route path="/coldplay">
+                        <Coldplay />
+                    </Route>
+                    <Route path="/details/:id">
+                        <Details />
+                    </Route>
+                    <Route path="/hidden">
+                        <Hidden />
+                    </Route>
+                    <Route path="/package">
+                        <Package />
+                    </Route>
+                    <Route path="/album/:name">
+                        <Album />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Container>
+        </Router>
+    );
 }
