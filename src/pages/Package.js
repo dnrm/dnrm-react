c07fa347cb1f699732294a92930616f0dbb19fc3
Daniel Medina <daniel@medina.com>
2021-04-24 12:47:32 -0500

    Added more info in package page

--- src/pages/Package.js
+++ src/pages/Package.js
@@ -6,6 +6,8 @@ const Package = () => (
     <h1>Hi</h1>
     <Brb />
     <p>ola</p>
+    <p>This page uses my own npm package which is just really a line break with extra steps. I wanted to try to make my own package</p>
+    <a href="https://www.npmjs.com/package/react-brb" target="_blank" rel="noreferrer">https://www.npmjs.com/package/react-brb</a>
   </>
 );
 
