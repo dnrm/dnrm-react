85b8af294e574345b3d41443641aaa8e2f3861a6
Daniel Medina <daniel@medina.com>
2021-04-30 18:35:58 -0500

    Updated button.js to use scss instead of styled components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-    padding: 15px 20px;
-    border: none;
-    -webkit-box-shadow: 0px 5px 15px 0px rgba(148,148,148,1);
-    -moz-box-shadow: 0px 5px 15px 0px rgba(148,148,148,1);
-    box-shadow: 0px 5px 15px 0px rgba(148,148,148,1);
-    border-radius: 5px;
-    margin: 10px;
-    background-color: ${props => props.danger ? '#d92a27' : 'white'};
-    color: black;
-`;
-
 
 export default function Button(props) {
     return (
-        <StyledButton onClick={props.onClick}>{props.children}</StyledButton>
+        <button class="button" onClick={props.onClick}>{props.children}</button>
     )
 }
