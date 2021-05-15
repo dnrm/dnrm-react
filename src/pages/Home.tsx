9ef5a29e025a171a1eea6841dd87e5b0f1414ef8
Daniel Medina <daniel@medina.com>
2021-05-15 10:01:45 -0500

    Added image to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 export default function Home() {
     return (
@@ -9,34 +9,38 @@ export default function Home() {
                 <title>Home | Daniel Medina</title>
             </Helmet>
             <section id="main">
-                <h1 className="text-4xl py-4 font-black">dnrm</h1>
+                <img
+                    src="https://danielmedina.dev/assets/v.jpg"
+                    alt="Daniel Medina"
+                    className="float-left w-36 h-36 mb-px object-cover rounded-full mr-4 border-white border-4"
+                />
+                <h1 className="text-4xl py-2">Daniel Medina</h1>
                 <hr />
-                <h3 className="text-2xl py-2 font-bold">Web Developer</h3>
-                <p>I am a web developer who likes working with JavaScript</p>
-                <p>
-                    I'm a web developer from Mexico. I like to work the most
-                    with javascript. I am really passionate about my coding
-                    projcets. I started back in January of 2020.
-                </p>
-                <br />
-                <p>
-                    Hello, my name is Daniel Medina and I am a full stack web
-                    developer. I work the most with JavaScript, primarily with
-                    Angular and NodeJs. I started my career in web development
-                    on January of 2020 before the coronavirus pandemic. The
-                    pandemic gave a lot of time to invest in learning
-                    technologies that would allow me to create a lot of content
-                    and digital media.
-                </p>
-                <br />
-                <p>
-                    I am a web developer from Mexico. I love programming and
-                    creating things. My JavaScript framework of choice is
-                    Angular. I started my journey in web development in January
-                    of 2020. I started learningHTML. After that I went and
-                    learned CSS and SASS. I also learned JavaScript. Now I am
-                    currently working on Angular.
-                </p>
+                <div className="about mt-2">
+                    <h3 className="text-2xl py-2 font-bold">Web Developer</h3>
+                    <p>
+                        I'm a web developer from Mexico. I like to work the most
+                        with javascript. I am really passionate about my coding
+                        projcets. I started back in January of 2020.
+                    </p>
+                    <p className="p-2">
+                        Hello, my name is Daniel Medina and I am a full stack
+                        web developer. I work the most with JavaScript,
+                        primarily with Angular and NodeJs. I started my career
+                        in web development on January of 2020 before the
+                        coronavirus pandemic. The pandemic gave a lot of time to
+                        invest in learning technologies that would allow me to
+                        create a lot of content and digital media.
+                    </p>
+                    <p className="p-2">
+                        I am a web developer from Mexico. I love programming and
+                        creating things. My JavaScript framework of choice is
+                        Angular. I started my journey in web development in
+                        January of 2020. I started learningHTML. After that I
+                        went and learned CSS and SASS. I also learned
+                        JavaScript. Now I am currently working on Angular.
+                    </p>
+                </div>
             </section>
         </Layout>
     );
