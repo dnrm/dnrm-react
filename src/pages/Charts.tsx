1f665d5e5ffabc9c6a04fb209912c6ffc932985c
Daniel Medina <daniel@medina.com>
2021-05-30 15:12:55 -0500

    Made the charts page show the two datasets.
    Found out the bug was caused due to the fact that both datasets had the same name.

--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -24,14 +24,14 @@ const Charts = () => {
         labels,
         datasets: [
             {
-                label: "idk growth",
+                label: "Other Things",
                 backgroundColor: "rgb(255, 99, 132)",
                 borderColor: "rgb(255, 99, 132)",
                 data: [1, 1.2, 3, 1.9, 5, 2, 6.5, 5.8, 15, 12.5, 20, 25],
                 fill: true,
             },
             {
-                label: "idk growth",
+                label: "Things",
                 backgroundColor: "#34a8eb",
                 borderColor: "#34a8eb",
                 data: [25, 4.7, 12.5, 8.5, 10, 4.2, 8, 2.4, 20, 1.5, 12, 28],
@@ -48,7 +48,8 @@ const Charts = () => {
                 scales: {
                     y: {
                         max: 30,
-                    },
+                        step: 1
+                    }
                 },
                 animations: {
                     y: {
