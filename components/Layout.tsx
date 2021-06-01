27d101225e497c21561dab28d4003a6385ef1843
Daniel Medina <daniel@medina.com>
2021-06-01 09:33:06 -0500

    Added layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Width = styled.div<any>`
+  width: 100%;
+  max-width: 768px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 15vh;
+  object-fit: cover;
+  margin-bottom: 2rem;
+`;
+
+const Layout = (props: any) => {
+  return (
+    <Container>
+      {props.img ? <Image src={props.img} alt="banner" /> : null}
+      <Width>{props.children}</Width>
+    </Container>
+  );
+};
+
+export default Layout;
