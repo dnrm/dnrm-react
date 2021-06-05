707520078ebfff8c00e0a9aa5709f7123efe49c5
Daniel Medina <daniel@medina.com>
2021-06-04 21:15:40 -0500

    Changed gray color palette

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface TrackProps {
     src: string,
     name: string,
@@ -8,7 +6,7 @@ interface TrackProps {
 
 const Music = (props: TrackProps) => {
     return (
-        <div className="bg-gray-700 shadow-lg rounded p-3">
+        <div className="bg-gray-800 shadow-lg rounded p-3">
             <div className="group relative">
                 <img
                     className="w-full md:w-72 block rounded"
