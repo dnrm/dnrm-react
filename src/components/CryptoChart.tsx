cf57c8fced0211380af083744bc1caf4808a9f90
Daniel Medina <daniel@medina.com>
2021-06-05 10:17:13 -0500

    :sparkles: Charts in charts.tsx page now get data from coincap api

---
+++ src/components/CryptoChart.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { Line } from 'react-chartjs-2';
+
+const CryptoChart = (props: any) => {
+
+    const [prices, setPrices] = useState<any>({});
+
+    useEffect(() => {
+        fetch(`https://api.coincap.io/v2/assets/${props.asset}/history?interval=d1`)
+            .then((r) => r.json())
+            .then((r) => {
+                let tenDayPrices = [];
+                let labels = [];
+                for (let i = r.data.length - 1; i >= r.data.length - 10; i--) {
+                    console.log(r.data[i].priceUsd)
+                    tenDayPrices.push(parseInt(r.data[i].priceUsd))
+                    labels.push(new Date(r.data[i].date).toLocaleString())
+                }
+                console.log(tenDayPrices)
+                setPrices({
+                    labels: labels,
+                    data: tenDayPrices
+                })
+            })
+    }, []);
+
+    let data = {
+        labels: prices.labels,
+        datasets: [
+            {
+                label: `${props.asset} price`,
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
+                data: prices.data,
+                fill: true,
+            },
+        ],
+    };
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    type: "linear",
+                    display: true,
+                    position: "left",
+                    id: "y-axis-1",
+                },
+            ],
+        },
+    };
+
+    return (
+        <Line data={data} options={options} type="line" />
+    )
+}
+
+export default CryptoChart;
--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -1,105 +1,67 @@
+import { useState, useEffect } from "react";
 import ChartJs from "../components/ChartJs";
 import Layout from "../components/Layout";
-import Line from "../components/Line";
+import { Line } from "react-chartjs-2";
+import CryptoChart from '../components/CryptoChart';
 
 const Charts = () => {
-    let line = [
-        {
-            id: "japan",
-            color: "hsl(91, 70%, 50%)",
-            data: [
+    const [prices, setPrices] = useState<any>({});
+
+    useEffect(() => {
+        fetch("https://api.coincap.io/v2/assets/bitcoin/history?interval=d1")
+            .then((r) => r.json())
+            .then((r) => {
+                let tenDayPrices = [];
+                let labels = [];
+                for (let i = r.data.length - 1; i >= r.data.length - 10; i--) {
+                    console.log(r.data[i].priceUsd);
+                    tenDayPrices.push(parseInt(r.data[i].priceUsd));
+                    labels.push(new Date(r.data[i].date).toLocaleString());
+                }
+                console.log(tenDayPrices);
+                setPrices({
+                    labels: labels,
+                    data: tenDayPrices,
+                });
+            });
+    }, []);
+
+    let data = {
+        labels: prices.labels,
+        datasets: [
+            {
+                label: "Bitcoin price",
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
+                data: prices.data,
+                fill: true,
+            },
+        ],
+    };
+
+    console.log(data);
+
+    const options = {
+        scales: {
+            yAxes: [
                 {
-                    x: "plane",
-                    y: 262,
-                },
-                {
-                    x: "helicopter",
-                    y: 93,
-                },
-                {
-                    x: "boat",
-                    y: 232,
-                },
-                {
-                    x: "train",
-                    y: 125,
-                },
-                {
-                    x: "subway",
-                    y: 114,
-                },
-                {
-                    x: "bus",
-                    y: 37,
-                },
-                {
-                    x: "car",
-                    y: 258,
-                },
-                {
-                    x: "moto",
-                    y: 30,
-                },
-                {
-                    x: "bicycle",
-                    y: 153,
-                },
-                {
-                    x: "horse",
-                    y: 120,
-                },
-                {
-                    x: "skateboard",
-                    y: 258,
-                },
-                {
-                    x: "others",
-                    y: 93,
+                    type: "linear",
+                    display: true,
+                    position: "left",
+                    id: "y-axis-1",
                 },
             ],
         },
-    ];
-
-    let data = {
-        labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ],
-        datasets: [
-            {
-                label: "Other Things",
-                backgroundColor: "rgb(255, 99, 132)",
-                borderColor: "rgb(255, 99, 132)",
-                data: [1, 1.2, 3, 1.9, 5, 2, 6.5, 5.8, 15, 12.5, 20, 25],
-                fill: true,
-            },
-            {
-                label: "Things",
-                backgroundColor: "#34a8eb",
-                borderColor: "#34a8eb",
-                data: [25, 4.7, 12.5, 8.5, 10, 4.2, 8, 2.4, 20, 1.5, 12, 28],
-                fill: true,
-            },
-        ],
     };
 
     return (
         <Layout>
             <h1 className="text-4xl pb-4">ChartJs</h1>
             <hr />
-            <Line data={line} />
-            <ChartJs data={data} />
+            {/* { prices != null ? <ChartJs data={data} /> : null} */}
             <br />
+            <CryptoChart asset="bitcoin" />
+            <CryptoChart asset="ethereum" />
         </Layout>
     );
 };
