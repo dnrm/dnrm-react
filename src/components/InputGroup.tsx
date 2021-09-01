3f16fe14f9ed479615e2455737108536b62a6552
Daniel Medina <daniel@medina.com>
2021-09-01 10:05:12 -0500

    :bug: Forgot to change hostname when creating user

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import "../styles/input-group.scss";
 import Input from "../components/Input";
 import Button from "../components/Button";
 
 export default function InputGroup(props: any) {
   return (
-    <div className="flex">
+    <div className="flex items-center">
       <form
         onSubmit={props.onSubmit}
         style={{ display: "flex", width: "100%" }}
